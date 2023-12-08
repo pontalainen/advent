0c6780c4e61049172e1712091035b1cbddd3ac18
pontalainen <pontusolsson03@gmail.com>
2023-12-08 19:07:18 +0100

    Working with lcm

--- day_8/script_2.js
+++ day_8/script_2.js
@@ -7,56 +7,36 @@ const nodes = nodesRaw.map((node) => {
         baseEnd: node.slice(2, 3),
         L: node.slice(7, 10),
         R: node.slice(12, 15),
-        previousEnd: null,
         turnsTaken: 0,
     };
 });
 
 const startNodes = nodes.filter((node) => node.baseEnd === 'A');
 
-let totalTotal = 0;
-
-// If not all nodes are on the same turn when having found a Z, this repeats
-for (let allSame = false; allSame === false; ) {
-    // Iterates over each starting node
+while (true) {
     startNodes.map((startNode) => {
         let currentNode;
         let totalTurns = 0;
 
-        // Restarts the turn iteration if the array ends
-        for (let done = false; done === false; ) {
-            // Iterates over all turns
-            turns.map((turn) => {
-                currentNode = nodes.find(
-                    (node) =>
-                        node.base ===
-                        (currentNode
-                            ? currentNode.previousEnd
-                                ? currentNode.previousEnd[turn]
-                                : currentNode[turn]
-                            : startNode[turn]),
-                );
-
-                totalTurns += 1;
-                if (currentNode.baseEnd === 'Z') {
-                    startNode.previousEnd = currentNode;
-                    startNode.turnsTaken += totalTurns;
-                    done = true;
-                }
-            });
+        while (true) {
+            const turn = turns[totalTurns % turns.length];
+            currentNode = nodes.find((node) => node.base === (currentNode ? currentNode[turn] : startNode[turn]));
+            totalTurns += 1;
+            if (currentNode.baseEnd === 'Z') {
+                startNode.turnsTaken = totalTurns;
+                break;
+            }
         }
     });
-
-    // A filtered array with only startNodes with the same turnsTaken as the first startNode
-    const allSameArray = startNodes.filter(
-        (node) => node.turnsTaken === startNodes[0].turnsTaken && startNodes[0].turnsTaken !== 0,
-    );
-    console.log(allSameArray);
-
-    // If all startNodes are included in the allSameArray the total turns taken has been found
-    if (allSameArray.length === startNodes.length) {
-        console.log(startNodes[0].turnsTaken);
-        allSame = true;
-    }
+    break;
 }
-console.log(totalTotal);
+
+const turnsArr = startNodes.map((node) => {
+    return node.turnsTaken;
+});
+
+const gcd = (a, b) => (a ? gcd(b % a, a) : b);
+
+const lcm = (a, b) => (a * b) / gcd(a, b);
+
+console.log(turnsArr.reduce(lcm));
