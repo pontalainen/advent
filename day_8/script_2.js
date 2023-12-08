34e74e0b6fc0d96544fbcd6edf6efc7a12db99f2
Pontalainen <pontusolsson03@gmail.com>
2023-12-08 14:36:56 +0100

    Comments, not really sure if it helps

--- day_8/script_2.js
+++ day_8/script_2.js
@@ -15,12 +15,16 @@ const nodes = nodesRaw.map((node) => {
 const startNodes = nodes.filter((node) => node.baseEnd === 'A');
 
 let totalTotal = 0;
+
+// If not all nodes are on the same turn when having found a Z, this repeats
 for (let allSame = false; allSame === false; ) {
+    // Iterates over each starting node
     startNodes.map((startNode) => {
         let currentNode;
         let totalTurns = 0;
-
+        // Restarts the turn iteration of the array ends
         for (let done = false; done === false; ) {
+            // Iterates over all turns
             turns.map((turn) => {
                 currentNode = nodes.find(
                     (node) =>
@@ -42,11 +46,13 @@ for (let allSame = false; allSame === false; ) {
         }
     });
 
+    // A filtered array with only startNodes with the same turnsTaken as the first startNode
     const allSameArray = startNodes.filter(
         (node) => node.turnsTaken === startNodes[0].turnsTaken && startNodes[0].turnsTaken !== 0,
     );
     console.log(allSameArray);
 
+    // If all startNodes are included in the allSameArray the total turns taken has been found
     if (allSameArray.length === startNodes.length) {
         console.log(startNodes[0].turnsTaken);
         allSame = true;
