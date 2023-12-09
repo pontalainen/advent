6c6ef0d2eab4e81bcf7492e731c6d300c070705d
pontalainen <pontusolsson03@gmail.com>
2023-12-09 14:44:15 +0100

    Linting and gitignore

--- day_1/part1.js
+++ day_1/part1.js
@@ -1,16 +1,10 @@
-import { data } from './data.js';
+import { data } from '../inputs/day1.js';
 
 let totalSum = 0;
-data.map((line) => {
+data.forEach((line) => {
     const lineArr = line.split('');
-    const lineInts = [
-        lineArr.filter((char) => {
-            return Number(char);
-        }),
-    ];
-    const sum = lineInts.map((line) => {
-        return line[0] + line[line.length - 1];
-    });
+    const lineInts = [lineArr.filter((char) => Number(char))];
+    const sum = lineInts.map((l) => l[0] + l[l.length - 1]);
     totalSum += Number(sum[0]);
 });
 console.log(totalSum);
